f091c7e78b5be7997d17d99473bc256466f75493
Skyler <s.vermeer@fhict.nl>
2020-12-20 23:12:02 +0100

    Cleanup + error handling no wolfram only wikipedia

--- PyDa/pyda.py
+++ PyDa/pyda.py
@@ -26,29 +26,30 @@ while True:
     if event == sg.WIN_CLOSED or event == 'Cancel': # ᕙ(`▿´)ᕗ if user closes window or clicks cancel ᕙ(`▿´)ᕗ
         break
 
-    try:
+    try: # ᕙ(`▿´)ᕗ Try to get results for both Wiki and Wolframᕙ(`▿´)ᕗ
         wiki_res = wp.summary(values[0],sentences=2)
         res = client.query(values[0])
         wolfram_res = next(res.results).text # ᕙ(`▿´)ᕗ print top wolframalpha results of inputᕙ(`▿´)ᕗ
         engine.say(wolfram_res)
         sg.PopupNonBlocking("Wolfram Result: " + wolfram_res, "Wikipedia Result: " + wiki_res)
-    except (wp.exceptions.DisambiguationError,wp.exceptions.PageError):
+    except (wp.exceptions.DisambiguationError,wp.exceptions.PageError): # ᕙ(`▿´)ᕗ Get only wolfram if wiki throws exceptions ᕙ(`▿´)ᕗ
         try:
             res = client.query(values[0])
             wolfram_res = next(res.results).text # ᕙ(`▿´)ᕗ print top wolframalpha results of inputᕙ(`▿´)ᕗ
             engine.say(wolfram_res)
             sg.PopupNonBlocking(wolfram_res)
-        except (StopIteration,AttributeError):
+        except (StopIteration,AttributeError): # ᕙ(`▿´)ᕗ And if wolfram also doesnt work, say that no results were foundᕙ(`▿´)ᕗ
             engine.say("No results found")
-
-    except:
+    except (StopIteration,AttributeError):
+        try:
+            wiki_res = wp.summary(values[0],sentences=2)
+            sg.PopupNonBlocking(wiki_res)
+        except:
+            engine.say("No results found")
+    except: # ᕙ(`▿´)ᕗ All the attributes inside your window. ᕙ(`▿´)ᕗ
         engine.say("No results found")
         sg.PopupNonBlocking("No results found")
     print('You entered ', values[0]) # ᕙ(`▿´)ᕗ Print input ᕙ(`▿´)ᕗ
-
-
-#    engine.say("Wikipedia Result: " + wiki_res)
     engine.runAndWait()
 
 window.close()
-
