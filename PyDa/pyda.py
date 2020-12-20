59cdf6f24bf76d7ca8de2df262d39d1b34419e65
Skyler <s.vermeer@fhict.nl>
2020-12-20 21:37:14 +0100

    Wolfram + Wikipedia combined result

--- PyDa/pyda.py
+++ PyDa/pyda.py
@@ -1,27 +1,34 @@
-import wolframalpha
-app_id = 'QW82JW-Q5U44TYEE9'  # get your own at https://products.wolframalpha.com/api/
-client = wolframalpha.Client(app_id)
-
-
-
+import wolframalpha as wa
 import PySimpleGUI as sg
+import wikipedia as wp
 
-sg.theme('DarkAmber')   # Add a touch of color
-# All the stuff inside your window.
+# ᕙ(`▿´)ᕗ Connect to wolfram ᕙ(`▿´)ᕗ
+app_id = 'QW82JW-Q5U44TYEE9'  # get your own at https://products.wolframalpha.com/api/
+client = wa.Client(app_id)
+
+
+sg.theme('DarkBlack1')   # ᕙ(`▿´)ᕗ Get the specific theme [Light/Dark][Colour][number optional] ᕙ(`▿´)ᕗ
+
+# ᕙ(`▿´)ᕗ All the attributes inside your window. ᕙ(`▿´)ᕗ
 layout = [
             [sg.Text('Enter a command'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
+            [sg.Button('Ok'), sg.Button('Cancel')]
+]
 
-# Create the Window
-window = sg.Window('JARVIS', layout)
-# Event Loop to process "events" and get the "values" of the inputs
+window = sg.Window('JARVIS', layout) # ᕙ(`▿´)ᕗ Create the Window ᕙ(`▿´)ᕗ
+
+# ᕙ(`▿´)ᕗ Event Loop to process "events" and get the "values" of the inputs ᕙ(`▿´)ᕗ
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+    if event == sg.WIN_CLOSED or event == 'Cancel': # ᕙ(`▿´)ᕗ if user closes window or clicks cancel ᕙ(`▿´)ᕗ
         break
-    print('You entered ', values[0])
-    res = client.query(values[0])
 
-    print(next(res.results).text)
+    print('You entered ', values[0]) # ᕙ(`▿´)ᕗ Print input ᕙ(`▿´)ᕗ
+    res = client.query(values[0])
+    
+    wolfram_res = next(res.results).text # ᕙ(`▿´)ᕗ print top wolframalpha results of inputᕙ(`▿´)ᕗ
+    wiki_res = wp.summary(values[0],sentences=3)
+    sg.Popup("Wolfram Result: " + wolfram_res, "Wikipedia Result: " + wiki_res)
 
 window.close()
+
